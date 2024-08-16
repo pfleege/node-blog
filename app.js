d2e55cde92ee115e3377d0cf43e1768614042206
Fleege <76089935+pfleege@users.noreply.github.com>
2024-08-16 17:43:27 +0300

    ADD template engine, ejs, to project
    - Delete old html -files and replace each with corresponding ejs files

--- app.js
+++ app.js
@@ -4,28 +4,27 @@ import { fileURLToPath } from "node:url";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-console.log(__dirname);
+/* console.log(__dirname); */
 
 const app = express();
+// Specify which view engine to use
+app.set("view engine", "ejs");
 
 app.listen(3000);
 
 app.get("/", (req, res) => {
-  res.sendFile("./views/index.html", { root: __dirname });
+  res.render("index");
 });
 
 app.get("/about", (req, res) => {
-  res.sendFile("./views/about.html", { root: __dirname });
+  res.render("about");
 });
 
-// Redirects
-
-app.get("/about-us", (req, res) => {
-  res.redirect("/about");
+app.get("/blogs/create", (req, res) => {
+  res.render("create");
 });
 
 // 404
-
 app.use((req, res) => {
-  res.status(404).sendFile("./views/404.html", { root: __dirname });
+  res.status(404).render("404");
 });
