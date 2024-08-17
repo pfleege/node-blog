568334d42048bfac3574b3857af45d0e4fa18eab
Fleege <76089935+pfleege@users.noreply.github.com>
2024-08-17 09:12:47 +0300

    ADD first dynamic content to views
    - Add a blogs object as test data to app.js and add it to index render
    - Add dynamic page titles to all render methods -> include in head

--- app.js
+++ app.js
@@ -1,30 +1,48 @@
 import express from "express";
-import { dirname } from "node:path";
-import { fileURLToPath } from "node:url";
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
-/* console.log(__dirname); */
 
 const app = express();
+
+// Temporary blog data
+
+const blogs = [
+  {
+    id: 1,
+    title: "Blog 1",
+    blogText:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt quo possimus est commodi doloremque ipsam laudantium suscipit, dolore dignissimos optio, minima quas aperiam cum omnis tenetur qui quidem reiciendis. Rerum ipsa nobis repellat porro maxime quis quam recusandae est.",
+  },
+  {
+    id: 2,
+    title: "Blog 2",
+    blogText:
+      "Lorem ipsum est commodi doloremque ipsam laudantium suscipit, dolore dignissimos optio, minima quas aperiam cum omnis tenetur qui quidem reiciendis.",
+  },
+  {
+    id: 3,
+    title: "Blog 3",
+    blogText:
+      "Lorem quo possimus est commodi doloremque ipsam laudantium suscipit, dolore dignissimos optio, minima quas aperiam cum omnis tenetur qui quidem reiciendis. Rerum ipsa nobis repellat porro maxime quis quam recusandae est.",
+  },
+];
+
 // Specify which view engine to use
 app.set("view engine", "ejs");
 
 app.listen(3000);
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", { title: "Home", blogs });
 });
 
 app.get("/about", (req, res) => {
-  res.render("about");
+  res.render("about", { title: "About" });
 });
 
 app.get("/blogs/create", (req, res) => {
-  res.render("create");
+  res.render("create", { title: "Add Blog" });
 });
 
 // 404
 app.use((req, res) => {
-  res.status(404).render("404");
+  res.status(404).render("404", { title: "404" });
 });
